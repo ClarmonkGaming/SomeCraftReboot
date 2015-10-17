fa5dafc97f4282ebc6f47de2ac76bf3a87757025
ClarmonkGaming <clarmonkgaming@gmail.com>
2015-10-17 18:57:58 -0400

    first commit

---
+++ src/main/java/com/cole/somecraft/SomeCraft.java
@@ -0,0 +1,34 @@
+package com.cole.somecraft;
+
+import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.common.event.FMLInitializationEvent;
+import net.minecraftforge.fml.common.event.FMLPostInitializationEvent;
+import net.minecraftforge.fml.common.event.FMLPreInitializationEvent;
+
+@Mod(modid="SomeCraft", name="SomeCraft", version="1.8-1.0" )
+public class SomeCraft
+{
+
+    @Mod.Instance("SomeCraft")
+    public static SomeCraft instance;
+
+    @Mod.EventHandler
+    public void preInit(FMLPreInitializationEvent event)
+    {
+
+    }
+
+    @Mod.EventHandler
+    public void init(FMLInitializationEvent event)
+    {
+
+    }
+
+    @Mod.EventHandler
+    public void postInit(FMLPostInitializationEvent event)
+    {
+
+    }
+
+
+}
